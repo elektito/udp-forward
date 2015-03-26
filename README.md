36bda1c1a8b3d47039675e3696633a276b47ca7d
Mostafa Razavi <mostafa@sepent.com>
2015-03-26 20:41:13 +0430

    Initial commit.
    udp-forward works but does not encrypt the reply.

---
+++ README.md
@@ -0,0 +1,26 @@
+# udp-forward
+
+udp-forward is a simple program for hiding the contents of certain UDP
+messages. The program simply listens to a UDP port, encrypts the
+datagrams and forwards them to the specified port.
+
+The encryption scheme used is by no means cryptographically strong. It
+is simply intended to hide the contents from unintelligent
+spoofers/sniffers.
+
+# DNS Forwarding
+
+One intention of udp-forward is hiding DNS messages from DNS
+spoofers. You can run udp-forward in a remote server by running
+`./udp-forward.py 0.0.0.0:5353 8.8.8.8:53`. This starts listening to port
+5353, runs the encryption scheme on whatever it receives and forwards
+the results to 8.8.8.8:53 (Google's public DNS server).
+
+You then need to run `sudo ./udp-forward.py 127.0.0.1:53
+remote-server:5353` on your local machine. The encryption scheme, when
+run twice, results in the original message, so this effectively
+forwards your DNS queries to Google's public DNS server.
+
+You can run the local instance of udp-forward on your home server and
+then setup your home DHCP server to return the home server as the
+local DNS server.
---
+++ udp-forward.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import argparse
+import SocketServer
+import socket
+
+lhost, lport = "", 5353
+fhost, fport = "", 53
+
+def encrypt(data):
+    char_encrypt = lambda x: 31 - x if x < 32 else \
+                             x if x == 32 else \
+                             159 - x if x < 127 else \
+                             127 if x == 127 else \
+                             383 - x
+    return ''.join([chr(char_encrypt(ord(ch))) for ch in data])
+
+class UdpHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        print 'got something'
+        data = self.request[0]
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(encrypt(data), (fhost, fport))
+        try:
+            response = sock.recv(1024)
+            self.request[1].sendto(response, self.client_address)
+        except socket.timeout:
+            print 'timeout'
+
+def main():
+    global lhost, lport, fhost, fport
+
+    parser = argparse.ArgumentParser(
+        description='UDP Forwarder.')
+
+    parser.add_argument(
+        'laddr', metavar='listen-address[:listen-port]',
+        help='The address and port to listen to. The port defaults to {}.'.format(lport))
+    parser.add_argument(
+        'faddr', metavar='forward-address[:forward-port]',
+        help='The address and port to forward the datagrams to. The port defaults to {}.'.format(fport))
+
+    args = parser.parse_args()
+
+    if ':' in args.laddr:
+        lhost, lport = args.laddr.split(':')
+        lport = int(lport)
+    else:
+        lhost = args.laddr
+
+    if ':' in args.faddr:
+        fhost, fport = args.faddr.split(':')
+        fport = int(fport)
+    else:
+        fhost = args.faddr
+
+    server = SocketServer.UDPServer((lhost, lport), UdpHandler)
+    try:
+        print 'Listening...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print 'Error:', e
+
+if __name__ == '__main__':
+    main()
